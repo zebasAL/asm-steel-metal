b26ee1ccfdb12b078d87f0f5bab25f5a8d0662b7
Sebastian Almeida <zebastian_al@hotmail.com>
2024-01-03 02:09:49 -0600

    video mp4 added

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -76,7 +76,7 @@ export default function Hero() {
         </IconButton>
       )}
     >
-      <VideoPlayer src={"/video/ASM_Drone.mp4"} />
+      <VideoPlayer src={"/videos/ASM_Drone.mp4"} />
 
       {/* <img
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
