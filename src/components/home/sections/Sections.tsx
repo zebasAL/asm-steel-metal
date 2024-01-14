dd883bf6b12b8f746169c016a626d0b73354763e
zebasAL <zebastian_al@hotmail.com>
2024-01-14 15:34:14 -0600

    rebase main branch

--- src/components/home/sections/Sections.tsx
+++ src/components/home/sections/Sections.tsx
@@ -1,19 +1,16 @@
 import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
-import { Carousel } from "@material-tailwind/react";
-import { useRef } from "react";
-import { useRouter } from 'next/router';
+import { Carousel, Typography } from "@material-tailwind/react";
+import { useRef, useState, useEffect } from "react";
+import { distributeItemsInCarouselColumns } from "~/utils/formatItemsForCarousel";
 import { SectionProduct } from "~/mock/products/sections";
 import { ImgWithBlurredCaption } from "~/components/ui"
-
 
 export default function Sections({ sections }: { sections: SectionProduct[] }) {
   const { t, lang } = useTranslation('home')
 
-  const carouselRef = useRef<HTMLSpanElement>(null)
-
   return (
-    <div className="relative backdrop-blur-20 bg-gradient-to-r from-gray-300 via-gray-50 to-white py-0 md:py-[100px]">
+    <div className="relative py-0 md:py-[100px]" style={{ background: 'linear-gradient(to right, #e0e0e0, #fafafa, #ffffff)' }}>
 
       <div className="flex flex-col lg/2:flex-row items-center text-sm font-medium text-gray-500 dark:text-gray-400 pb-[10px] md:pb-[20px] mx-[10px] sm:mx-20">
         <h3 className="mt-10 lg:mt-0 text-2xl md:text-3xl lg:text-4xl mb-[0] text-center md:text-left items-center text-black w-auto whitespace-normal md:whitespace-nowrap">
@@ -21,48 +18,7 @@ export default function Sections({ sections }: { sections: SectionProduct[] }) {
         </h3>
       </div>
 
-      <Carousel
-        loop
-        // autoplay
-        // autoplayDelay={10000}
-        color="black"
-        key={0}
-        navigation={({ setActiveIndex, activeIndex, length }) => (
-          <div className="absolute bottom-4 left-2/4 flex -translate-x-2/4 gap-2">
-            {new Array(length).fill("").map((_, i) => (
-              <span
-                itemID="tabs"
-                ref={i === 0 ? carouselRef : null}
-                tabIndex={i}
-                key={i}
-                className={`
-                    block h-1 cursor-pointer rounded-2xl transition-all content-[''] bg-black
-                    ${activeIndex === i ? "w-8" : "w-4"}
-                  `}
-                onClick={() => setActiveIndex(i)}
-              />
-            ))}
-          </div>
-        )}
-      >
-        <div className="mx-[10px] md:mx-[50px] grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-10">
-          {sections.map((section: SectionProduct) => (
-            <Link
-              key={section.title}
-              href={"/sections/" + section.title}
-              hrefLang={lang}
-              className="relative group h-[300px] lg:h-96 w-full rounded-xl hover:shadow-lg hover:shadow-gray-500 transition group cursor-pointer"
-            >
-              <ImgWithBlurredCaption
-                image={section.image_primary}
-                secondaryImage={section.image_secondary}
-                alt={`section-${section}`}
-                title={section.title}
-              />
-            </Link>
-          ))}
-        </div>
-      </Carousel>
+      <SectionsCarrousel sections={sections} />
 
       <div className="hidden md:block">
         <div
@@ -106,6 +62,110 @@ export default function Sections({ sections }: { sections: SectionProduct[] }) {
           }}
         />
       </div>
+    </div >
+  )
+}
+
+// -----------------------------------------------------------------------------
+
+function SectionsCarrousel({ sections }: { sections: SectionProduct[] }) {
+  const sectionsGroup = distributeItemsInCarouselColumns(sections);
+
+  const [elements, setElements] = useState<JSX.Element[]>([])
+
+  const carouselRef = useRef<HTMLSpanElement>(null)
+
+  useEffect(() => {
+    const itemsList = (sectionsGroup ?? []).map((group, index) => (
+      <div key={index} className="mx-[10px] md:mx-[50px] grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-10">
+        {group.map((section: SectionProduct) => (
+          <Link
+            key={section.title}
+            href={"/sections/" + section.title}
+            className="relative group rounded-xl hover:shadow-lg hover:shadow-gray-500 transition cursor-pointer"
+          >
+            <ImgWithBlurredCaption
+              image={section.image_primary}
+              secondaryImage={section.image_secondary}
+              alt={`section-${section}`}
+              title={section.title}
+            />
+          </Link>
+        ))}
+      </div>
+    ))
+
+    if (carouselRef.current) {
+      carouselRef.current.click()
+    }
+
+    setElements(itemsList)
+  }, [sections])
+
+  return (
+    <div className="home-products-carousel">
+      {elements?.length > 0
+        ? (
+          <Carousel
+            loop
+            // autoplay
+            // autoplayDelay={10000}
+            color="black"
+            key={0}
+            navigation={({ setActiveIndex, activeIndex, length }) => (
+              <div className="absolute bottom-4 left-2/4 flex -translate-x-2/4 gap-2">
+                {new Array(length).fill("").map((_, i) => (
+                  <span
+                    itemID="tabs"
+                    ref={i === 0 ? carouselRef : null}
+                    tabIndex={i}
+                    key={i}
+                    className={`
+                    block h-1 cursor-pointer rounded-2xl transition-all content-[''] bg-black
+                    ${activeIndex === i ? "w-8" : "w-4"}
+                  `}
+                    onClick={() => setActiveIndex(i)}
+                  />
+                ))}
+              </div>
+            )}
+          >
+            {elements}
+          </Carousel>
+        )
+        : (
+          <div className="mx-[10px] md:mx-[50px] grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-10">
+            <Typography
+              as="div"
+              variant="h1"
+              className="mb-4 w-full h-52	sm:h-64 md:h-80 lg:h-96 bg-transparent hidden lg:block relative group hover:shadow-lg hover:shadow-gray-500 transition cursor-pointer"
+            >
+              &nbsp;
+            </Typography>
+            <Typography
+              as="div"
+              variant="h1"
+              className="mb-4 w-full h-52	sm:h-64 md:h-80	 lg:h-96 bg-transparent hidden md:block relative group hover:shadow-lg hover:shadow-gray-500 transition cursor-pointer"
+            >
+              &nbsp;
+            </Typography>
+            <Typography
+              as="div"
+              variant="h1"
+              className="mb-4 w-full h-52	sm:h-64 md:h-80	 lg:h-96 bg-transparent relative group hover:shadow-lg hover:shadow-gray-500 transition cursor-pointer"
+            >
+              &nbsp;
+            </Typography>
+            <Typography
+              as="div"
+              variant="h1"
+              className="mb-4 w-full h-52	sm:h-64 md:h-80	 lg:h-96 bg-transparent relative group hover:shadow-lg hover:shadow-gray-500 transition cursor-pointer"
+            >
+              &nbsp;
+            </Typography>
+          </div>
+        )
+      }
     </div>
   )
 }
